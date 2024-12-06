856923501d2b1d1e7ffb6ff37b971e4490ebfd4d
Sharif-11 <sharifsharifulislam061@gmail.com>
2024-12-06 22:20:50 +0600

    Loading effect added - Products Title added

--- src/Components/Products.tsx
+++ src/Components/Products.tsx
@@ -3,22 +3,28 @@ import Product from "./Product";
 
 const Products = () => {
   const { products, loading, error } = useProducts();
+  console.log({ products });
   if (loading) {
-    return <div className="my-8 mx-5">Loading...</div>;
+    return (
+      <span className="loading loading-bars loading-xl m-8 flex justify-center mx-auto"></span>
+    );
   }
   if (error) {
     return <div className="my-8 mx-5">Error: {error}</div>;
   }
   return (
-    <div className="my-8 mx-5 flex flex-wrap justify-center gap-8">
-      {products.map((product) => (
-        <Product
-          key={product.id}
-          name={product.title}
-          price={product.price}
-          url={product.url}
-        />
-      ))}
+    <div className="my-8 mx-5">
+      <h3 className="text-center mt-5 mb-8 font-bold text-3xl">All Products</h3>
+      <div className="flex flex-wrap justify-center gap-8">
+        {products.map((product) => (
+          <Product
+            key={product.id}
+            name={product.title}
+            price={product.price}
+            url={product.url}
+          />
+        ))}
+      </div>
     </div>
   );
 };
